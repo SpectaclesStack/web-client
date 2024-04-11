416f1d92678d48e3b3be48363cb26dd5f1143eaa
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-11 23:10:55 +0200

    Added add new question feature

--- BlazorClient/BlazorClient/Model/Question.cs
+++ BlazorClient/BlazorClient/Model/Question.cs
@@ -1,14 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorClient.Model
 {
     public class Question
     {
-        public Question() { }
         public int QuestionId { get; set; }
         public int userid { get; set; }
-        public required string Title { get; set; }
-        public required string Body { get; set; }
-        public required DateTime CreateAt { get; set; }
+        //[Required]
+        public string ? Title { get; set; }
+        //[Required]
+        public string ? Body { get; set; }
+        public DateTime ? CreateAt { get; set; }
     }
 }
