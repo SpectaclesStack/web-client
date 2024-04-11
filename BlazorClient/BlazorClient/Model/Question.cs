ca052811d8b846fb3662001c2b604c169b5ee8dd
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-11 15:59:15 +0200

    added Question Service

---
+++ BlazorClient/BlazorClient/Model/Question.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorClient.Model
+{
+    public class Question
+    {
+        public Question() { }
+        public int QuestionId { get; set; }
+        public int userid { get; set; }
+        public required string Title { get; set; }
+        public required string Body { get; set; }
+        public required DateTime CreateAt { get; set; }
+    }
+}
