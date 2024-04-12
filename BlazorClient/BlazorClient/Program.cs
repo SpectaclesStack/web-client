771d138081a293b738a1f61c380edecd3fa5bc20
Farisani Paul Ratshikombo <farisanipaul.ratshikombo@vcontractor.co.za>
2024-04-12 11:25:28 +0200

    almost final changes now

--- BlazorClient/BlazorClient/Program.cs
+++ BlazorClient/BlazorClient/Program.cs
@@ -1,5 +1,6 @@
 using BlazorClient.Client.Pages;
 using BlazorClient.Components;
+using BlazorClient.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddScoped<IIdService, IdService>();
 
 var app = builder.Build();
 
